cd026d833f8625a255802c371f98abbb0c609117
BlueIceWatermelon <X1248270035@foxmail.com>
2026-06-29 12:36:27 +0800

    feat(c):新增实体 + Mapper + Service

---
+++ gateway/src/main/java/com/powerreport/gateway/mapper/SysUserMapper.java
@@ -0,0 +1,12 @@
+package com.powerreport.gateway.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.powerreport.gateway.entity.SysUserEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 系统用户 Mapper
+ */
+@Mapper
+public interface SysUserMapper extends BaseMapper<SysUserEntity> {
+}
