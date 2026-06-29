45fcd8cb0c86e8ff5119841208c9f9e895576fb6
pluto <1658614797@qq.com>
2026-06-29 22:52:27 +0800

    fix: add swagger ui and test dependencies

--- qa-common/src/test/java/com/myenglish/qacommon/QaCommonApplicationTests.java
+++ qa-common/src/test/java/com/myenglish/qacommon/QaCommonApplicationTests.java
@@ -1,9 +1,7 @@
 package com.myenglish.qacommon;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class QaCommonApplicationTests {
 
     @Test
